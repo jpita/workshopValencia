7d8253c7b6cc0615cf3d26b60a63616eb557a1f9
Jose Pita <dvpita@gmail.com>
2019-09-24 20:29:28 +0200

    appium server checker and screenshooter

---
+++ src/test/java/appium/AppiumServerChecker.java
@@ -0,0 +1,52 @@
+package appium;
+
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.net.UrlChecker;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppiumServerChecker {
+    private static AppiumDriverLocalService service = null;
+
+
+    public static void startAppiumServerWithEmulator(String emulatorName) {
+        service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+                .withArgument(() -> "--avd", emulatorName));
+
+        service.start();
+    }
+
+    public static void startAppiumServer(URL appiumServerURL) {
+        try {
+            if (!isAppiumRunning(appiumServerURL)) {
+                service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder());
+                service.start();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+
+    }
+
+    public static void stopAppiumServer(){
+        try{
+            service.stop();
+        }catch (NullPointerException ex)
+        {
+            System.out.println("Service wasn't running");
+        }
+    }
+
+
+    private static Boolean isAppiumRunning(URL server_url) throws Exception {
+        final URL url = new URL(server_url + "/sessions");
+        try {
+            new UrlChecker().waitUntilAvailable(300, TimeUnit.MILLISECONDS, url);
+            return true;
+        } catch (UrlChecker.TimeoutException e) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/appium/Screenshooter.java
@@ -0,0 +1,22 @@
+package appium;
+
+import io.appium.java_client.AppiumDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Screenshooter {
+    public static void takeScreenshot(AppiumDriver driver){
+        File file = driver.getScreenshotAs(OutputType.FILE);
+
+        // the filename is the folder name on test.screenshot.path property plus the completeTestName
+        try {
+            FileUtils.copyFile(file,
+                    new File("screenshot.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

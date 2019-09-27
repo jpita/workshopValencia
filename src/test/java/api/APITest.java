3d9a2a5fb0bb7b1495f0f429d4929ef136dbbf2d
Jose Pita <dvpita@gmail.com>
2019-09-27 15:49:40 +0200

    getSearchResult

--- src/test/java/api/APITest.java
+++ src/test/java/api/APITest.java
@@ -1,20 +1,23 @@
 package api;
 
-import org.testng.annotations.Test;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class APITest {
-    private static String baseURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Nelson%20Mandela&utf8=&format=json";
+    private static String baseURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json";
 
-    @Test
-    public void getFirstBookingAndCheckFirstName() {
+    public String getSearchResult(String query) {
     //then().body("title", equalTo("Nelson Mandela"));
-        String a = (String) given().get(baseURL).then()
+        return (String) given().get(baseURL+"&srsearch="+query).then()
                 .extract()
                 .jsonPath()
                 .getList("query.search.title").get(0);
-        Assert.assertEquals(a, "Nelson Mandela");
+    }
+
+    @Test
+    public void testURL(){
+        Assert.assertEquals(getSearchResult("Nelson Mandela"), "Nelson Mandela");
     }
 }

fab0b146eacc0ecda5909226ba22b1491f8bbc11
Jose Pita <dvpita@gmail.com>
2019-10-01 12:16:12 +0200

    make test fail

--- src/test/java/appium/tests/OnBoardingTests.java
+++ src/test/java/appium/tests/OnBoardingTests.java
@@ -16,7 +16,7 @@ public class OnBoardingTests extends BaseTest{
     public void checkOnBoardingTest(){
         FirstOnBoardingPage firstOnBoardingPage = new FirstOnBoardingPage(driver);
         Screenshooter.takeScreenshot(driver);
-        Assert.assertTrue(firstOnBoardingPage.checkForOnBoardingImagePresence(),
+        Assert.assertTrue(false,
                 "OOPS! image is not showing");
         Assert.assertTrue(firstOnBoardingPage.isFirstOnBoardingPageTitleCorrect(),
                 "OOPS! title is not correct");
